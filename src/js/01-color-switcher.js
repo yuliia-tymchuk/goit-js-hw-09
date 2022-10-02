cd53dc89e5af2edca26f31a91f5f4b0c8bc17d8e
lia2307 <juliatymchuk23@gmail.com>
2022-10-03 00:55:02 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+// stopBtn.setAttribute('disabled', 'true');
+// stopBtn.classList.add('is-inactive');
+
+setAttributeBtn();
+
+function onStartClick() {
+  refs.startBtn.disabled = true;
+  setAttributeBtn();
+
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  refs.startBtn.disabled = false;
+  setAttributeBtn();
+
+  clearInterval(intervalId);
+}
+
+function setAttributeBtn() {
+  refs.stopBtn.disabled = true;
+  if (refs.startBtn.disabled) {
+    refs.stopBtn.disabled = false;
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
